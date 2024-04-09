f146e0abd143166cff2696efdf7a1c30f31630f8
blank <hayaken3007@i.softbank.jp>
2024-04-10 01:51:11 +0900

    toast コンテナ作成 #4

---
+++ src/containers/toast-container.tsx
@@ -0,0 +1,18 @@
+import { ToastContainer as Container } from 'react-toastify';
+
+export const ToastContainer = () => {
+  return (
+    <Container
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  );
+};
