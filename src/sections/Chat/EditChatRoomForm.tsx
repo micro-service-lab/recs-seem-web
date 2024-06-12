67824b37604c44ac240ebd0d532774abc3d8321e
Hayashi Kenta <hayaken3007@i.softbank.jp>
2024-06-12 18:58:14 +0900

    fix #4

--- src/sections/Chat/EditChatRoomForm.tsx
+++ src/sections/Chat/EditChatRoomForm.tsx
@@ -207,7 +207,7 @@ export const EditChatRoomForm = ({ chatRoom, onSuccess }: Props) => {
       <LoadingButton
         loading={isSubmitting || isPending || uploadIsPending}
         type="submit"
-        className="btn btn-gradient !mt-6 border-0 uppercase shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]"
+        className="btn btn-primary !mt-6 border-0"
         loadingChildren={<>{t("updating")}</>}
       >
         {t("update-chat-room")}
