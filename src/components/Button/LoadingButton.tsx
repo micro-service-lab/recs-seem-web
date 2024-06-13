5cde57b12407d1d0bd02fd663066e8b9eab8a3c3
blank <hayaken3007@i.softbank.jp>
2024-06-13 14:43:33 +0900

    fix msg send button #4

--- src/components/Button/LoadingButton.tsx
+++ src/components/Button/LoadingButton.tsx
@@ -3,16 +3,24 @@ import IconLoader from "../Icon/IconLoader";
 
 type Props = HTMLProps<HTMLButtonElement> & {
   loading: boolean;
-  loadingChildren: ReactNode;
+  loadingChildren?: ReactNode;
   children: ReactNode;
   type: "button" | "submit" | "reset" | undefined;
-}
+};
 
-export const LoadingButton = ({loading, loadingChildren, children, type, ...other}: Props) => {
+export const LoadingButton = ({
+  loading,
+  loadingChildren,
+  children,
+  type,
+  ...other
+}: Props) => {
   return (
-    <button type={type}  {...other}>
-      {loading && <IconLoader className="animate-[spin_2s_linear_infinite] inline-block align-middle ltr:mr-2 rtl:ml-2 shrink-0" />}
-      {loading ? loadingChildren : children}
+    <button type={type} {...other}>
+      {loading && (
+        <IconLoader className="animate-[spin_2s_linear_infinite] inline-block align-middle ltr:mr-2 rtl:ml-2 shrink-0" />
+      )}
+      {loading ? loadingChildren || <></> : children}
     </button>
   );
 };
